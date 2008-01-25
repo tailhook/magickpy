cee242f830bc3e994b5bdb6b06ca904eace6cc86
pc <pc@092e8ac9-94ca-dc11-bd90-001d7d91e118>
2008-01-25 11:53:08 +0000

    Added files, forgotten in previous commit Implemented:  * Image.draw - equal to "-draw" command-line option
    git-svn-id: svn://svn.gafol.net/magickpy/trunk@4 092e8ac9-94ca-dc11-bd90-001d7d91e118

---
+++ magickpy/draw.py
@@ -0,0 +1,106 @@
+
+__all__ = [
+    'SegmentInfo',
+    'AffineMatrix',
+    'GradientInfo',
+    ]
+
+import ctypes
+from magickpy.image import PImage
+from magickpy.types import RectangleInfo, PixelPacket
+from magickpy.enums import CompositeOp
+from magickpy import lib
+
+class SegmentInfo(ctypes.Structure):
+    _fields_ = [
+        ('x1', ctypes.c_double),
+        ('y1', ctypes.c_double),
+        ('x2', ctypes.c_double),
+        ('y2', ctypes.c_double),
+        ]
+
+class AffineMatrix(ctypes.Structure):
+    _fields_ = [
+        ('sx', ctypes.c_double),
+        ('rx', ctypes.c_double),
+        ('ry', ctypes.c_double),
+        ('sy', ctypes.c_double),
+        ('tx', ctypes.c_double),
+        ('ty', ctypes.c_double),
+        ]
+
+class GradientInfo(ctypes.Structure):
+    _fields_ = [
+        ('type', ctypes.c_int),
+        ('bounding_box', RectangleInfo),
+        ('gradient_vector', SegmentInfo),
+        ('stops', ctypes.c_void_p),
+        ('number_stops', ctypes.c_ulong),
+        ('spread', ctypes.c_int),
+        ('debug', ctypes.c_int),
+        ('signature', ctypes.c_ulong),
+        ]
+
+class ElementReference(ctypes.Structure):
+    pass
+ElementReference._fields_ = [
+        ('id', ctypes.c_char_p),
+        ('type', ctypes.c_int),
+        ('gradient', GradientInfo),
+        ('signature', ctypes.c_ulong),
+        ('previous', ctypes.POINTER(ElementReference)),
+        ('next', ctypes.POINTER(ElementReference)),
+    ]
+
+class DrawInfo(ctypes.Structure):
+    _fields_ = [
+        ('primitive', ctypes.c_char_p),
+        ('geometry', ctypes.c_char_p),
+        ('viewbox', RectangleInfo),
+        ('affine', AffineMatrix),
+        ('gravity', ctypes.c_int),
+        ('fill', PixelPacket),
+        ('stroke', PixelPacket),
+        ('stroke_width', ctypes.c_double),
+        ('gradient', GradientInfo),
+        ('fill_pattern', PImage),
+        ('tile', PImage),
+        ('stroke_pattern', PImage),
+        ('stroke_antialias', ctypes.c_int),
+        ('text_antialias', ctypes.c_int),
+        ('fill_fule', ctypes.c_int),
+        ('linecap', ctypes.c_int),
+        ('linejoin', ctypes.c_int),
+        ('miterlimit', ctypes.c_ulong),
+        ('dash_offset', ctypes.c_double),
+        ('decorate', ctypes.c_int),
+        ('compose', ctypes.c_int),
+        ('text', ctypes.c_char_p),
+        ('face', ctypes.c_ulong),
+        ('font', ctypes.c_char_p),
+        ('metrics', ctypes.c_char_p),
+        ('family', ctypes.c_char_p),
+        ('style', ctypes.c_int),
+        ('stretch', ctypes.c_int),
+        ('weight', ctypes.c_ulong),
+        ('encoding', ctypes.c_char_p),
+        ('pointsize', ctypes.c_double),
+        ('density', ctypes.c_char_p),
+        ('align', ctypes.c_int),
+        ('undercolor', PixelPacket),
+        ('border_color', PixelPacket),
+        ('server_name', ctypes.c_char_p),
+        ('dash_pattern', ctypes.c_double),
+        ('clip_mask', ctypes.c_char_p),
+        ('bounds', SegmentInfo),
+        ('clip_units', ctypes.c_int),
+        ('opacity', ctypes.c_short),
+        ('render', ctypes.c_int),
+        ('element_reference', ElementReference),
+        ('debug', ctypes.c_int),
+        ('signature', ctypes.c_ulong),
+        ]
+    def __new__(C):
+        return ctypes.cast(lib.AcquireDrawInfo(), ctypes.POINTER(DrawInfo)).contents
+    def __del__(self):
+        lib.DestroyDrawInfo(ctypes.byref(self))
---
+++ tests/color.py
@@ -0,0 +1,23 @@
+import unittest
+
+class TestRGB(unittest.TestCase):
+
+    def setUp(self):
+        global Color
+        from magickpy import Color
+
+    def testRgb(self):
+        c = Color.rgb(0.5, 0.5, 0.5)
+        self.assertEquals('<Color: rgba(32767,32767,32767,0)>', repr(c))
+
+    def testBlue(self):
+        c = Color.named("blue")
+        self.assertEquals('<Color: rgba(0,0,65535,0)>', repr(c))
+
+    def testYellow(self):
+        c = Color.named("yellow")
+        self.assertEquals('<Color: rgba(65535,65535,0,0)>', repr(c))
+
+    def testStr(self):
+        c = Color.named("yellow")
+        self.assertEquals('#ffff0000', str(c))
